db8f7260488c6de9c5dce1ebb602f18e3f88472e
Bailey Frye <baileyafrye@comcast.net>
2025-05-08 12:08:35 -0500

    Updated review model and created empty review controller class

---
+++ Controllers/ReviewController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+public class ReviewController : ControllerBase
+{
+    
+}
--- Models/Review.cs
+++ Models/Review.cs
@@ -7,33 +7,25 @@ namespace api.Models
 	[Table("reviews")]
 	public class Review : BaseModel
 	{
-		[PrimaryKey("id", false)]
+		[PrimaryKey("id")]
 		public int Id { get; set; }
 
 		[Reference(typeof(Cocktail))]
 		public Cocktail Cocktail { get; set; } = new Cocktail { };
 
-		[Column("cocktailId")]
+		[Column("cocktail_id")]
 		public int CocktailId { get; set; }
 
 		[Column("rating")]
 		public int Rating { get; set; }
 
-		[Column("comment")]
-		public string Comment { get; set; } = string.Empty;
-
-		[Column("authorName")]
-		public string AuthorName { get; set; } = string.Empty;
-
-		[Column("authorImageUrl")]
-		public string AuthorImageUrl { get; set; } = string.Empty;
-
-		[Column("createdAt")]
+		[Column("user_id")]
+		public string UserId { get; set; } = string.Empty;
+		
+		[Column("created_at")]
 		public DateTime CreatedAt { get; set; }
 
-		[Column("updatedAt")]
+		[Column("updated_at")]
 		public DateTime UpdatedAt { get; set; }
-
-		public Review() { }
 	}
 }
